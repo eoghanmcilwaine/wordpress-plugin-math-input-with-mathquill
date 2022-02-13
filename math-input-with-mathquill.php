ffeca4bef8fe2a1bbbc153c99cec7a739d9dcd16
Eoghan McIlwaine <mcilwaine@gmail.com>
2022-02-13 21:26:25 +0100

    Use wordpress.org handle for author

--- math-input-with-mathquill.php
+++ math-input-with-mathquill.php
@@ -5,7 +5,7 @@
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.1
- * Author:            Eoghan McIlwaine  <mcilwaine@gmail.com>
+ * Author:            eoghanmcilwaine
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       direct-math-input

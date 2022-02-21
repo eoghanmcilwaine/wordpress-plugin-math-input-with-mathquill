7947769b31654becdd486e29820efe8a4ff66b9f
Eoghan McIlwaine <mcilwaine@gmail.com>
2022-02-21 16:28:19 +0100

    Show latex in separate block below editor

--- src/edit.js
+++ src/edit.js
@@ -105,6 +105,19 @@ const BlockControlsForMath = ({ callEditorMethod, latex }) => (
 	</BlockControls>
 );
 
+const CopyableLatexString = ({ latex }) => {
+	const pre = useRef();
+	const selectContents = () => {
+		window.getSelection().selectAllChildren(pre.current)
+	};
+
+	return (
+		<pre tabIndex="0" className="copyable-latex-string" ref={pre} onClick={selectContents}>{
+			unwrapBlockDelims(latex)
+		}</pre>
+	);
+}
+
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
@@ -137,6 +150,7 @@ export default function Edit({ attributes, setAttributes }) {
 					setLatex={updateLatex}
 					updateRef={updateRef}
 				/>
+				<CopyableLatexString latex={latex} />
 			</p>
 		</Fragment>
 	);
